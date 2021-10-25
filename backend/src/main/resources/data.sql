e3221093e2937d9b04975da1bf2752a6b6fef134
BrunoRiguetto <bruno.riguetto@gmail.com>
2021-10-25 10:47:03 -0300

    Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,3 +17,8 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('BootCamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2022-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2022-12-20T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://thumbs.dreamstime.com/b/businessman-showing-white-presentation-board-cartoon-flat-vector-illustration-concept-isolated-background-102429468.jpg', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas duvidas', 2, 'https://thumbs.dreamstime.com/b/businessman-showing-white-presentation-board-cartoon-flat-vector-illustration-concept-isolated-background-102429468.jpg', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://thumbs.dreamstime.com/b/businessman-showing-white-presentation-board-cartoon-flat-vector-illustration-concept-isolated-background-102429468.jpg', 0, 1);
+
